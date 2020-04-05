15f78470b558d45f2c6962dfcf085ae459e05ff6
ConanShin <conan.shin@sk.com>
2020-04-05 19:12:38 +0900

    removed unnecessary logging on kakao authorization

--- src/utils/Kakao.js
+++ src/utils/Kakao.js
@@ -18,26 +18,21 @@ class KakaoConnector {
     login() {
         return new Promise((resolve, reject) => {
             const success = function(response) {
-                console.log('succes??')
                 Kakao.Auth.setAccessToken(response.access_token)
-                console.log('success access token', response.access_token, this)
                 resolve(response.access_token)
             }
             const fail = function(error) {
                 alert(error)
                 reject(error)
             }
-            console.log('login trying')
             Kakao.Auth.login({
                 success,
                 fail
             })
-            console.log('login tried!')
         })
     }
     logout() {
         Kakao.Auth.logout(function() {
-            alert('success log out')
             Promise.resolve()
         })
     }
